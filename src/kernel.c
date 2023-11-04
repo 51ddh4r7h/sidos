ea46c6856d285aba8654a7dc615443bf6796b55d
51ddh4r7h <aelpulasiddharth@gmail.com>
2023-11-04 17:18:18 +0530

    Successfully implemented heap

--- src/kernel.c
+++ src/kernel.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "idt/idt.h"
-#include "io/io.h"
+#include "memory/heap/kheap.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -20,7 +20,7 @@ void terminal_putchar(int x, int y, char c, char colour)
 
 void terminal_writechar(char c, char colour)
 {
-    if(c == '\n')
+    if (c == '\n')
     {
         terminal_row += 1;
         terminal_col = 0;
@@ -32,24 +32,23 @@ void terminal_writechar(char c, char colour)
     if (terminal_col >= VGA_WIDTH)
     {
         terminal_col = 0;
-        terminal_row += 1; 
+        terminal_row += 1;
     }
 }
-
 void terminal_initialize()
 {
     video_mem = (uint16_t*)(0xB8000);
     terminal_row = 0;
     terminal_col = 0;
-    for (int y=0; y<VGA_HEIGHT; y++)
+    for (int y = 0; y < VGA_HEIGHT; y++)
     {
-        for (int x=0; x<VGA_WIDTH; x++)
+        for (int x = 0; x < VGA_WIDTH; x++)
         {
             terminal_putchar(x, y, ' ', 0);
         }
-    }
-    
+    }   
 }
+
 
 size_t strlen(const char* str)
 {
@@ -62,10 +61,10 @@ size_t strlen(const char* str)
     return len;
 }
 
-void print(const char*str)
+void print(const char* str)
 {
     size_t len = strlen(str);
-    for(int i=0; i<len; i++)
+    for (int i = 0; i < len; i++)
     {
         terminal_writechar(str[i], 15);
     }
@@ -74,9 +73,11 @@ void print(const char*str)
 void kernel_main()
 {
     terminal_initialize();
-    print("Hello World!\ntest");
+    print("Hello world!\ntest");
 
+    // Initialize the heap
+    kheap_init();
+
+    // Initialize the interrupt descriptor table
     idt_init();
-
-    enable_interrupts();
 }
