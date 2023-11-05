add38bedfe84ce15d75ae54a8ebcd714862d6376
51ddh4r7h <aelpulasiddharth@gmail.com>
2023-11-05 19:51:11 +0530

    Starting to create FAT file system

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -3,21 +3,45 @@ BITS 16
 
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
-_start:
-    jmp short start
-    nop
- times 33 db 0
+
+jmp short start
+nop
+
+; FAT16 Header
+OEMIdentifier           db 'SIDOS   '
+BytesPerSector          dw 0x200
+SectorsPerCluster       db 0x80
+ReservedSectors         dw 200
+FATCopies               db 0x02
+RootDirEntries          dw 0x40
+NumSectors              dw 0x00
+MediaType               db 0xF8
+SectorsPerFat           dw 0x100
+SectorsPerTrack         dw 0x20
+NumberOfHeads           dw 0x40
+HiddenSectors           dd 0x00
+SectorsBig              dd 0x773594
+
+; Extended BPB (Dos 4.0)
+DriveNumber             db 0x80
+WinNTBit                db 0x00
+Signature               db 0x29
+VolumeID                dd 0xD105
+VolumeIDString          db 'SIDOS BOOTL'
+SystemIDString          db 'FAT16   '
+
+
 start:
     jmp 0:step2
 
 step2:
-    cli ;Clear Interrupts
+    cli ; Clear Interrupts
     mov ax, 0x00
     mov ds, ax
     mov es, ax
     mov ss, ax
     mov sp, 0x7c00
-    sti ;Enables Interrupts
+    sti ; Enables Interrupts
 
 .load_protected:
     cli
@@ -26,37 +50,37 @@ step2:
     or eax, 0x1
     mov cr0, eax
     jmp CODE_SEG:load32
-
+    
 ; GDT
 gdt_start:
 gdt_null:
     dd 0x0
     dd 0x0
 
-; offser 0x8
-gdt_code:     ;CS should point to this
-    dw 0xffff ;Segment limit first 0-15 bits
-    dw 0      ;Base first 0-15 bits
-    db 0      ;Base 16-23 bits
-    db 0x9a   ;Access Bytes
-    db 11001111b ;High 4 bit flags and the low 4 bit flags
-    db 0      ; Base 24-31 bits
+; offset 0x8
+gdt_code:     ; CS SHOULD POINT TO THIS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x9a   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
 
-; Offset 0x10
-gdt_data:
-    dw 0xffff ;Segment limit first 0-15 bits
-    dw 0      ;Base first 0-15 bits
-    db 0      ;Base 16-23 bits
-    db 0x92   ;Access Bytes
-    db 11001111b ;High 4 bit flags and the low 4 bit flags
-    db 0      ; Base 24-31 bits
+; offset 0x10
+gdt_data:      ; DS, SS, ES, FS, GS
+    dw 0xffff ; Segment limit first 0-15 bits
+    dw 0      ; Base first 0-15 bits
+    db 0      ; Base 16-23 bits
+    db 0x92   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0        ; Base 24-31 bits
 
 gdt_end:
 
 gdt_descriptor:
     dw gdt_end - gdt_start-1
     dd gdt_start
-
+ 
  [BITS 32]
  load32:
     mov eax, 1
@@ -66,39 +90,39 @@ gdt_descriptor:
     jmp CODE_SEG:0x0100000
 
 ata_lba_read:
-    mov ebx, eax, ;Backup the lba
-    ;Send the highest 8 bits of the lba to hard disk controller
+    mov ebx, eax, ; Backup the LBA
+    ; Send the highest 8 bits of the lba to hard disk controller
     shr eax, 24
-    or eax, 0xE0 ; Selects the master
+    or eax, 0xE0 ; Select the  master drive
     mov dx, 0x1F6
     out dx, al
-    ; Finished sending highest 8 bits of the lba
-    
+    ; Finished sending the highest 8 bits of the lba
+
     ; Send the total sectors to read
     mov eax, ecx
     mov dx, 0x1F2
     out dx, al
-    ; Finished sending the total sectors
-    
-    ; send more bits of lba
-    mov eax, ebx ; Restore the backup lba
+    ; Finished sending the total sectors to read
+
+    ; Send more bits of the LBA
+    mov eax, ebx ; Restore the backup LBA
     mov dx, 0x1F3
     out dx, al
-    ; Finished sending more bits of the lba
+    ; Finished sending more bits of the LBA
 
-    ; send more bits of the lba 
+    ; Send more bits of the LBA
     mov dx, 0x1F4
-    mov eax, ebx ; Restore the backup lba
+    mov eax, ebx ; Restore the backup LBA
     shr eax, 8
     out dx, al
-    ; Finished sending more bits of the lba
+    ; Finished sending more bits of the LBA
 
-    ; Send upper 16 bits of the lba
+    ; Send upper 16 bits of the LBA
     mov dx, 0x1F5
-    mov eax, ebx ; Restore the backup lba
+    mov eax, ebx ; Restore the backup LBA
     shr eax, 16
     out dx, al
-    ; Finished sending upper 16 bits of the lba
+    ; Finished sending upper 16 bits of the LBA
 
     mov dx, 0x1f7
     mov al, 0x20
@@ -107,7 +131,8 @@ ata_lba_read:
     ; Read all sectors into memory
 .next_sector:
     push ecx
-; Check if we need to read
+
+; Checking if we need to read
 .try_again:
     mov dx, 0x1f7
     in al, dx
@@ -115,22 +140,13 @@ ata_lba_read:
     jz .try_again
 
 ; We need to read 256 words at a time
-    mov ecx,256
+    mov ecx, 256
     mov dx, 0x1F0
     rep insw
     pop ecx
     loop .next_sector
-    ;End of reading sectors into memory
+    ; End of reading sectors into memory
     ret
-
-
-
-
-
-
-
 
 times 510-($ - $$) db 0
 dw 0xAA55
-
-
