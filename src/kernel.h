994b936957dc43ada5d580eab519adec4da208b5
51ddh4r7h <aelpulasiddharth@gmail.com>
2023-11-05 14:22:28 +0530

    Creating path parser

--- src/kernel.h
+++ src/kernel.h
@@ -1,11 +1,12 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-
-
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 20
 
+#define SIDOS_MAX_PATH 108
+
 void kernel_main();
-void print(const char*str);
-#endif 
+void print(const char* str);
+
+#endif
--- src/status.h
+++ src/status.h
@@ -1,9 +1,9 @@
 #ifndef STATUS_H
 #define STATUS_H
 
-
-#define SIDOS_ALL_OS 0
+#define PEACHOS_ALL_OK 0
 #define EIO 1
 #define EINVARG 2
 #define ENOMEM 3
+#define EBADPATH 4
 #endif
