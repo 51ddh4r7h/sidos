5fa96ce89aaf1ebbb97671eed8e21869c810f1f7
51ddh4r7h <aelpulasiddharth@gmail.com>
2023-11-11 11:12:46 +0530

    Implementing malloc in our stdlib

--- programs/stdlib/src/sidos.h
+++ programs/stdlib/src/sidos.h
@@ -1,5 +1,10 @@
 #ifndef SIDOS_H
 #define SIDOS_H
+#include <stddef.h>
+
 void print(const char* filename);
+int getkey();
+
+void* sidos_malloc(size_t size);
 
 #endif
---
+++ programs/stdlib/src/stdlib.c
@@ -0,0 +1,12 @@
+#include "stdlib.h"
+#include "sidos.h"
+
+void* malloc(size_t size)
+{
+    return sidos_malloc(size);
+}
+
+void free(void* ptr)
+{
+
+}
