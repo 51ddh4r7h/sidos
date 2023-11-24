1e598e012721c71d98406a8c11b5c51e1a519327
51ddh4r7h <aelpulasiddharth@gmail.com>
2023-11-24 14:03:42 +0530

    Process Arguments-1

--- programs/stdlib/src/sidos.h
+++ programs/stdlib/src/sidos.h
@@ -3,6 +3,13 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+
+struct command_argument
+{
+    char argument[512];
+    struct command_argument* next;
+};
+
 void print(const char* filename);
 int sidos_getkey();
 
@@ -12,5 +19,6 @@ void sidos_putchar(char c);
 int sidos_getkeyblock();
 void sidos_terminal_readline(char* out, int max, bool output_while_typing);
 void sidos_process_load_start(const char* filename);
+struct command_argument* sidos_parse_command(const char* command, int max);
 
 #endif
